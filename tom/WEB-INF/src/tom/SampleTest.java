53a3139ce918b2ce374845afc681264c4ae53516
ashokkumar0307 <b.ashokkumar0307@gmail.com>
2016-09-23 20:04:07 +0530

    added new project

---
+++ tom/WEB-INF/src/tom/SampleTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package tom;
+
+/**
+ * @author ashok
+ *
+ */
+public class SampleTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello tom");
+
+	}
+
+}
